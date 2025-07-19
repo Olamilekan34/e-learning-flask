65785d1f37d0bdb5258e8897cc6bede230f0c784
Olamilekan Osunneye <osunneyeolamilekan9@gmail.com>
2025-07-19 07:02:13 +0100

    updated config.py to render database

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'os.environ.get("postgresql://elearning_db_9rod_user:uOWjZXOhIzWrySok7qBCZsWiepPFTvgC@dpg-d1tii4re5dus73dk5q0g-a/elearning_db_9rod", "sqlite:///default.db")'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://elearning_db_9rod_user:uOWjZXOhIzWrySok7qBCZsWiepPFTvgC@dpg-d1tii4re5dus73dk5q0g-a/elearning_db_9rod")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'a7d11f9fcb14e359a348fa8b15ea021c60b90618af85c5dacb98544bc5e21f03'
